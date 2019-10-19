aeb9e7bd30c14f30e27bd79bca21b03c3a69f656
Jonas Wanke <contact@jonas-wanke.com>
2019-10-19 21:43:17 +0200

    refactor(parser): make Scanner static

--- parser/lib/src/scanner/scanner.dart
+++ parser/lib/src/scanner/scanner.dart
@@ -6,129 +6,125 @@ import 'token.dart';
 export 'token.dart';
 
 class Scanner {
-  Scanner({
-    @required this.source,
-    @required this.errorCollector,
-  })  : assert(source != null),
-        assert(errorCollector != null);
+  Scanner._();
 
-  final String source;
-  final tokens = <Token>[];
-  final ErrorCollector errorCollector;
+  static List<Token> scan({
+    @required String source,
+    @required ErrorCollector errorCollector,
+  }) {
+    assert(source != null);
+    assert(errorCollector != null);
+    final tokens = <Token>[];
 
-  // The current state.
-  int _start = 0;
-  int _current = 0;
-  int _line = 1;
+    int _start = 0;
+    int _current = 0;
+    int _line = 1;
 
-  bool get _isAtEnd => _current >= source.length;
+    bool isAtEnd() => _current >= source.length;
+    String peek() => isAtEnd() ? '\0' : source[_current];
+    String advance() => source[_current++];
+    String advanceWhile(bool Function(String char) predicate) {
+      while (predicate(peek())) {
+        advance();
+      }
+      return source.substring(_start, _current);
+    }
 
-  void scanTokens() {
-    while (!_isAtEnd) {
+    void addToken(TokenType type, [dynamic literal]) {
+      tokens.add(Token(
+        type: type,
+        line: _line,
+        lexeme: source.substring(_start, _current),
+        literal: literal,
+      ));
+    }
+
+    void parseDecimalNumber() {
+      final number = advanceWhile(_isDecimalDigit);
+      addToken(TokenType.NUMBER, int.parse(number));
+    }
+
+    void parseHexNumber() {
+      final number = advanceWhile(_isHexDigit);
+      addToken(TokenType.NUMBER, int.parse(number, radix: 16));
+    }
+
+    void parseComment() {
+      final comment = advanceWhile((c) => c != '\n');
+      addToken(TokenType.COMMENT, comment);
+    }
+
+    void parseIdentifier() {
+      final identifier = advanceWhile(_isLetterDigitUnderscore);
+      addToken(TokenType.IDENTIFIER, identifier);
+    }
+
+    void scanToken() {
+      final c = advance();
+      const singleCharTokens = {
+        '(': TokenType.LEFT_PAREN,
+        ')': TokenType.RIGHT_PAREN,
+        ',': TokenType.COMMA,
+        '.': TokenType.DOT,
+        '-': TokenType.MINUS,
+        '+': TokenType.PLUS,
+        '#': TokenType.NUMBER_SIGN,
+        ':': TokenType.COLON,
+      };
+
+      if (singleCharTokens.containsKey(c)) {
+        addToken(singleCharTokens[c]);
+        return;
+      }
+
+      if (_isDecimalDigit(c)) {
+        parseDecimalNumber();
+        return;
+      }
+
+      if (c == '\$') {
+        parseHexNumber();
+        return;
+      }
+
+      if (c == '*') {
+        parseComment();
+        return;
+      }
+
+      if (' \t\r'.contains(c)) {
+        return;
+      }
+
+      if (c == '\n') {
+        _line++;
+        return;
+      }
+
+      if (_isLetterDigitUnderscore(c)) {
+        parseIdentifier();
+        return;
+      }
+
+      errorCollector.add(Error(
+        line: _line,
+        message: 'Unexpected character $c.',
+      ));
+    }
+
+    while (!isAtEnd()) {
       _start = _current;
-      _scanToken();
+      scanToken();
     }
-
     tokens.add(Token(type: TokenType.EOF, line: _line));
+
+    return tokens;
   }
 
-  String _advance() => source[_current++];
+  static bool _isDecimalDigit(String c) => '1234567890'.contains(c);
+  static bool _isHexDigit(String c) => '1234567890ABCDEFabcdef'.contains(c);
 
-  String _advanceWhile(bool Function(String char) predicate) {
-    while (predicate(_peek())) {
-      _advance();
-    }
-    return source.substring(_start, _current);
-  }
-
-  String _peek() => _isAtEnd ? '\0' : source[_current];
-
-  void _scanToken() {
-    final c = _advance();
-    const singleCharTokens = {
-      '(': TokenType.LEFT_PAREN,
-      ')': TokenType.RIGHT_PAREN,
-      ',': TokenType.COMMA,
-      '.': TokenType.DOT,
-      '-': TokenType.MINUS,
-      '+': TokenType.PLUS,
-      '#': TokenType.NUMBER_SIGN,
-      ':': TokenType.COLON,
-    };
-
-    if (singleCharTokens.containsKey(c)) {
-      _addToken(singleCharTokens[c]);
-      return;
-    }
-
-    if (_isDecimalDigit(c)) {
-      _parseDecimalNumber();
-      return;
-    }
-
-    if (c == '\$') {
-      _parseHexNumber();
-      return;
-    }
-
-    if (c == '*') {
-      _parseComment();
-      return;
-    }
-
-    if (' \t\r'.contains(c)) {
-      return;
-    }
-
-    if (c == '\n') {
-      _line++;
-      return;
-    }
-
-    if (_isLetterDigitUnderscore(c)) {
-      _parseIdentifier();
-      return;
-    }
-
-    errorCollector.add(Error(
-      line: _line,
-      message: 'Unexpected character $c.',
-    ));
-  }
-
-  void _addToken(TokenType type, [dynamic literal]) {
-    tokens.add(Token(
-      type: type,
-      line: _line,
-      lexeme: source.substring(_start, _current),
-      literal: literal,
-    ));
-  }
-
-  bool _isDecimalDigit(String c) => '1234567890'.contains(c);
-  bool _isHexDigit(String c) => '1234567890ABCDEFabcdef'.contains(c);
-
-  bool _isLetterDigitUnderscore(String c) =>
+  static bool _isLetterDigitUnderscore(String c) =>
       'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_'.contains(c) ||
       _isDecimalDigit(c);
-
-  void _parseDecimalNumber() {
-    final number = _advanceWhile(_isDecimalDigit);
-    _addToken(TokenType.NUMBER, int.parse(number));
-  }
-
-  void _parseHexNumber() {
-    final number = _advanceWhile(_isHexDigit);
-    _addToken(TokenType.NUMBER, int.parse(number, radix: 16));
-  }
-
-  void _parseComment() {
-    final comment = _advanceWhile((c) => c != '\n');
-    _addToken(TokenType.COMMENT, comment);
-  }
-
-  void _parseIdentifier() {
-    final identifier = _advanceWhile(_isLetterDigitUnderscore);
-    _addToken(TokenType.IDENTIFIER, identifier);
-  }
 }
