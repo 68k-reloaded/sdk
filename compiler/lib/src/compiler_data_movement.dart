37b430c93ecd34d355c251834b89570f79f9bfc8
Jonas Wanke <contact@jonas-wanke.com>
2020-02-14 19:06:34 +0100

    feat(compiler): compile MOVE

--- compiler/lib/src/compiler_data_movement.dart
+++ compiler/lib/src/compiler_data_movement.dart
@@ -10,6 +10,40 @@ final dataMovementCompilers = {
   OperationType.moveq: _compileMoveq,
 };
 
+final _moveBits = [0, 0].bits;
+CompiledStatement _compileMove(Operation statement) {
+  assert(statement.type == OperationType.move);
+  assert(statement.operands.length == 2);
+
+  final source = statement.operands.first;
+  final destination = statement.operands.second;
+
+  assert(
+      destination.type != OperandType.ccr, 'MOVE to CCR is not yet supported');
+  assert(source.type != OperandType.sr, 'MOVE from SR is not yet supported');
+  assert(destination.type != OperandType.sr, 'MOVE to SR is not yet supported');
+  assert(source.type != OperandType.usp, 'MOVE from SR is not yet supported');
+  assert(
+      destination.type != OperandType.usp, 'MOVE to SR is not yet supported');
+
+  assert(operandTypesAll.contains(source.type));
+  assert(operandTypesNoAxPcImm.contains(destination));
+
+  final sizeBits = statement.compiledSizeOneBased;
+  final destRegisterBits = destination.compiledRegister;
+  final destModeBits = destination.compiledMode;
+  final srcModeBits = source.compiledMode;
+  final srcRegisterBits = source.compiledRegister;
+  return CompiledStatement(
+    _moveBits +
+        sizeBits +
+        destRegisterBits +
+        destModeBits +
+        srcModeBits +
+        srcRegisterBits,
+  );
+}
+
 final _moveqBits = [0, 1, 1, 1].bits;
 CompiledStatement _compileMoveq(Operation statement) {
   assert(statement.type == OperationType.moveq);
@@ -23,6 +57,5 @@ CompiledStatement _compileMoveq(Operation statement) {
 
   final registerBits = destination.compiledRegister;
   final dataBits = (immediate as ImmediateOperand).compiledByteBits;
-
   return CompiledStatement(_moveqBits + registerBits + [0].bits + dataBits);
 }
--- compiler/test/compiler_data_movement_test.dart
+++ compiler/test/compiler_data_movement_test.dart
@@ -7,6 +7,50 @@ import 'statement_utils.dart';
 
 void main() {
   group('Compiler compiles single data movement statement', () {
+    group('MOVE:', () {
+      <String, StatementWithExpectedResult>{
+        'MOVE.B #0,D1': StatementWithExpectedResult(
+          type: OperationType.move,
+          size: SizeValue.byte,
+          operands: [immediate(0), dx(1)],
+          expectedResult: CompiledStatement(
+            [0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0].bits,
+            immediateOrSourceExtensions: [Bits.word(0x0000)],
+          ),
+        ),
+        'MOVE.W #1,D2': StatementWithExpectedResult(
+          type: OperationType.move,
+          size: SizeValue.word,
+          operands: [immediate(1), dx(2)],
+          expectedResult: CompiledStatement(
+            [0, 0, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0].bits,
+            immediateOrSourceExtensions: [Bits.word(0x0001)],
+          ),
+        ),
+        'MOVE.W D2,D3': StatementWithExpectedResult(
+          type: OperationType.move,
+          size: SizeValue.word,
+          operands: [dx(2), dx(3)],
+          expectedResult: CompiledStatement(
+            [0, 0, 1, 1, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0].bits,
+          ),
+        ),
+        'MOVE.L #\$10004042, D3': StatementWithExpectedResult(
+          type: OperationType.move,
+          size: SizeValue.longWord,
+          operands: [immediate(0x42), dx(3)],
+          expectedResult: CompiledStatement(
+            [0, 0, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0].bits,
+            immediateOrSourceExtensions: [
+              Bits.word(0x1000),
+              Bits.word(0x4042),
+            ],
+          ),
+        ),
+      }.forEach((name, stmt) {
+        test(name, () => stmt.test());
+      });
+    });
     group('MOVEQ:', () {
       <String, StatementWithExpectedResult>{
         'MOVEQ.L #0, D0': StatementWithExpectedResult(
